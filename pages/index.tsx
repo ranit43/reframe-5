472df0e105add4f35ae334dd4c3efbb8e095af76
Ranit Debnath Akash <ranitid12@gmail.com>
2022-06-07 20:14:27 +0600

    Sync project code! Time: Tue Jun 07 2022 20:14:26 GMT+0600 (Bangladesh Standard Time)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,53 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardImage,
+  Center,
+  Cluster,
+  Cover,
+  Image,
+  Link,
+  Sidebar,
+  Spacer,
+  Stack,
+  Switch,
+  Switcher,
+  Text,
+} from "@reframe.so/blocks";
 
-    import { Stack } from "@reframe.so/blocks";
-  
-    export default function App() {
-      return (
-        <Stack data-reframe-id="root" />
-      )
-    }
-  
+export default function App() {
+  return (
+    <Stack
+      data-reframe-id="root"
+      direction={"column"}
+      recursive={false}
+      splitAfter={"-1"}
+      alignItems={"stretch"}
+      space={1}
+      padding={0}
+    >
+      <Text
+        data-reframe-id="b3457020-2b41-442d-9f70-6e56b0deb965"
+        h1={false}
+        h2={false}
+        h3={false}
+        h4={false}
+        h5={false}
+        h6={false}
+        b={false}
+        small={false}
+        transform={"none"}
+        i={false}
+        span={false}
+        del={false}
+        em={false}
+        blockquote={false}
+        color={"default"}
+        align={"left"}
+      >
+        Abc
+      </Text>
+    </Stack>
+  );
+}
